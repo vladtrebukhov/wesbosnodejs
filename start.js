a96c0d5105c605599da72ab39f23a480ea3d9e34
Vlad Trebukhov <vlad.trebukhov123@gmail.com>
2018-12-26 18:12:17 -0500

    initial starter files

---
+++ start.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// import environmental variables from our variables.env file
+require('dotenv').config({
+  path: 'variables.env'
+});
+
+// Connect to our Database and handle an bad connections
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises for async/await
+mongoose.connection.on('error', err => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
